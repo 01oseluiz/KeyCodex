33dd53a339ebd33bf448a5a77960a35e768d927d
André Laranjeira <andrecaldaslaranjeira@gmail.com>
2017-09-01 11:08:44 -0300

    Adição da lista de TODOs do módulo Methods.

--- src/main/scala/Methods/Methods.scala
+++ src/main/scala/Methods/Methods.scala
@@ -2,4 +2,13 @@ package Methods
 
 class Methods {
 
+  /*TODO list:
+
+      TODO Unmarked spaces method
+      TODO Most marked spaces method
+      TODO Most weighted spaces method
+      TODO Capitalization specification for each method
+
+   */
+
 }
