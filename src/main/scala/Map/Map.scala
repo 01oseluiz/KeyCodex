25b54d3c1313d053294e1d76010bab48f7de84a6
Hugo-NF <hugonfonseca@hotmail.com>
2017-09-25 23:10:10 -0300

    "Interface definition with Methods module"

--- src/main/scala/Map/Map.scala
+++ src/main/scala/Map/Map.scala
@@ -1,7 +1,35 @@
 package Map
 
-class Map {
-  val elements : List[String] = ()
-  val size : Int = elements.size
-  val groupSize : Int = elements.head.size
+import scala.collection.immutable
+import scala.collection.mutable._
+import scala.io.Source
+import scala.io.StdIn._
+/**
+  TODO Criar indexação do mapa e assimilar para o HashMap (Rotação a partir do A)
+  TODO Criar lista de pesos seguindo a indexação do mapa
+  TODO Criar método getWeights()
+ */
+case class Group(characters: List[Char], weight: Int)
+
+class Map (){
+  private val elements : List[Group] = ()
+  private var index : HashMap[Char, List[Int]] = HashMap.empty
+
+  def getGroupSize : Int = {
+    elements.head.characters.size
+  }
+  def getWeightedMap(password : String) : List[Group]
+
+  def mapEntries(): Unit = {
+    val file = Source.fromFile("MapEntries.txt")
+    var currentEntry : List[Int] = List.empty
+
+    for (i <- file.getLines()){
+      currentEntry :+= i.toInt
+    }
+
+  }
+
+    //file.getLines.foreach{currentEntry += this}
 }
+
