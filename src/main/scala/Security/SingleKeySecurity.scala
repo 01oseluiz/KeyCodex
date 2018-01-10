3988e545d114045859dd8c1351187963e303cc66
José Luiz <joseluizgnogueira@live.com>
2018-01-10 14:32:04 -0200

    verificação de melhor senha

--- src/main/scala/Security/SingleKeySecurity.scala
+++ src/main/scala/Security/SingleKeySecurity.scala
@@ -2,27 +2,18 @@ package Security
 
 import com.nulabinc.zxcvbn.{Strength, Zxcvbn}
 
-/* TODO-Frodo: List task
-*     Pesquisar Metodos de verificação de segurança
-*       EX: testedesenha.com
-*           howsecureismypassword.net
-* */
-
-class Security {
+class SingleKeySecurity(pass:String) {
 
   private val zxcvbn = new Zxcvbn
-  private var password:String = ""
-  private var pass_strength:Strength = _
+  private var password:String = pass
+  private var pass_strength:Strength = zxcvbn.measure(password)
 
   def Password:String = password
   def Password_= (x:String) {password = x; pass_strength = zxcvbn.measure(password)}
 
-  def getStrength_Double:Double = pass_strength.getCrackTimeSeconds.getOnlineNoThrottling10perSecond
+  def getStrength:Double = pass_strength.getCrackTimeSeconds.getOnlineNoThrottling10perSecond
 
   def getCrackTime:String = pass_strength.getCrackTimesDisplay.getOnlineNoThrottling10perSecond
 
   def getFeedback:String = pass_strength.getFeedback.getWarning + ": " + pass_strength.getFeedback.getSuggestions()
-
-  def getStrength_Percent:Double = 0
-
 }
