fc4c05a143aef6deef63975ff6288574971acdfb
AndreLaranjeira <andrecaldaslaranjeira@gmail.com>
2018-08-08 17:00:15 -0300

    Ported Group and BinaryOps classes.

---
+++ app/src/main/kotlin/br/com/unb/keycodex/map/Group.kt
@@ -0,0 +1,3 @@
+package br.com.unb.keycodex.map
+
+data class Group(var characters: String, var weight: Int)
---
+++ app/src/main/kotlin/br/com/unb/keycodex/methods/BinaryOps.kt
@@ -0,0 +1,29 @@
+package br.com.unb.keycodex.methods
+
+/*
+ * Singleton para abrigar operações realizáveis em números binários.
+ */
+
+object BinaryOps {
+
+    /*
+     * O método CountOnes só funciona para inteiros positivos. Isso se deve ao fato de que os pesos
+     * utilizados no programa serão sempre positivos e ao fato de que existem diversas representações
+     * binárias possíveis para números negativos.
+     */
+
+    fun CountOnes(number: Int): Int {
+
+        // O número deve ser positivo, conforme explicado anteriormente.
+
+        assert(number >= 0)
+
+        return when {
+            (number == 0) -> 0
+            (number % 2 == 1) -> 1 + CountOnes(number/2)
+            else -> CountOnes(number/2)
+        }
+
+    }
+
+}
